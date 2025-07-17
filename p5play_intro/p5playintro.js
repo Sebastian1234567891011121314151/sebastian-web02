b6d14d9bad0bad3865d51f3f806c6258cdf35ed3
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2025-07-17 10:25:49 +0000

    Jul 17, 2025, 10:25 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -11,7 +11,7 @@ function setup() {
   fill("lightblue");
   strokeWeight (10);
   stroke ("red")
-  circle(50,50,60)
+  circle(30,30,60)
 //  ball = new.Sprite ();
 //  ball.diameter = 100;
 //  ball.color = (navyblue);
