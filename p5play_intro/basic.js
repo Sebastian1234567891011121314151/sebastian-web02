c94e349aab9180d1258afc5efa6ab833072a5c55
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2025-07-31 10:35:47 +0000

    Jul 31, 2025, 10:35 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -1,19 +1,99 @@
 function setup() {
   // write your codes here
+new Canvas (900,400)
+background("limegreen")
+// python print
+console.log("Hello i am here");
+
+let a =5;
+let b =10;
+
+let sum= a+b;
+textSize(40);
+text("Hello:"+sum ,150,150);
+
+
 
   // --- Exercise: Area of Triangle ---
   // write your codes here
+//   let width= 200;
+//   let height= 150;
+//   let area= 1/2 *width*height;
 
-  // --- Exercise: Sum of first 10 even numbers ---
-  // write your codes here
+//   text("area of triangle:" +area,150,180);
+
+//   console.log("Area of Triangle =",area)
+
+
+//   // --- Exercise: Sum of first 10 even numbers ---
+//   // write your codes here
+// // i=i+(1)=i++(plus 1) if 2 or above then 1+=(num)vise versa minus
+//   let sum1 =0
+// for (let i=2 ; i<21; i=i+2){
+// console.log("Counting..."+i );
+// sum1=sum1+i;
+// }
+// console.log(sum1)
+// console.log("stopped the loop");
 
   // --- Exercise: Age category classification ---
   // write your codes here
-
+// let age= 
+// if(age<=6){
+//   console.log("Kindergarten");
+// }
+// else if (age<=9){
+//   console.log("Lower Primary");
+// }
+// else if (age<=12){
+//   console.log("Upper Primary");
+// }
+// else{
+//   console.log("Secondary");
+// }
   // --- Exercise: Display odd numbers backward using while loop ---
   // write your codes here
-
+// let num=19
+// while(num>0){
+//   console.log(num);
+//   num-=2;
+// }
   // --- Exercise: Array operations (groceries) ---
   // write your codes here
+let zoo = [];
+zoo[0]="Monkey";
+zoo.push("Sebastian");// join the queue at the end of the list
+zoo.push("Flamingo");
+console.log(zoo);
+console.log("How many animals?",zoo.length);
+
+zoo.push("Goat")
+for (let i =0; i<zoo.length;i++){
+  console.log(zoo[i])
+}
+let groceries=["apple","bread", "milk"];
+groceries.push("orange");
+groceries.push("butter");
+groceries[1]="kaya";
+groceries.shift(0);
+console.log(groceries);
+
+textSize(40);
+
+let y = 180;
+for(let item of groceries){
+  text("Groceries: "+item ,150,y);
+  y=y+45;
 }
 
+
+
+
+
+} // this is the function setup bracket
+
+
+
+function draw(){
+
+}
--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -2,7 +2,7 @@ let floor;
 let ball;
 function setup() {
   // Set up the canvas
-  new Canvas(300,300);
+  new Canvas(1000,600);
   background("blue"); //background color
 
   // Basic shape testing
@@ -22,17 +22,18 @@ function setup() {
  ball = new Sprite ();
  ball.diameter = 100;
  ball.color = "pink";
- ball.y =50;
- ball.x =50
- ball.vel.x=5;
-ball.bounciness= 0.99
+ ball.y =0;
+ ball.x =300
+ ball.vel.x=1;
+  ball.vel.y=3;
+ball.bounciness= 1.1
 
-world.gravity.y = 10
+// world.gravity.y = 10
 
  floor= new Sprite ();
- floor.w =800;
+ floor.w =100;
  floor.h =25
- floor.y=300
+ floor.y=550
  floor.collider= "static"
   // End Basic shape testing
 
@@ -44,9 +45,22 @@ world.gravity.y = 10
 function draw() {
   background("red");
 if (ball.x>width){
-  ball.vel.x=-1.1*ball.vel.x;
+  ball.vel.x=-1*ball.vel.x;
   }
 if (ball.x<0){
-  ball.vel.x=-1.1*ball.vel.x;
+    ball.vel.x=-1*ball.vel.x;
 }
+
+
+if (ball.y>height){
+    ball.vel.y=-1*ball.vel.y;
+}
+
+  
+if (ball.y<0){
+    ball.vel.y=-1*ball.vel.y;
+}
+
+floor.x = mouseX;
+floor.y = mouseY;
 }
