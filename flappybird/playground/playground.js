d1345b209536bb7d8855f10086912a03f4c2a76f
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2025-08-07 09:44:03 +0000

    Aug 7, 2025, 9:44 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -9,10 +9,11 @@ base = loadImage('assets/base.png');
 function setup(){
 new Canvas(400,600);
 background("gray");
-
+new floor= new Sprite();
+floor.img=base;
 }
 
 function draw(){
 image(bg,0,0,width,height);
-image(base,0,500,width,height)
+image(floor0,500,width,height);
 }
