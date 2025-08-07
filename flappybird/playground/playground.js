5058fa6465c5859c9c99f4b749f58638ce5ce5bc
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2025-08-07 09:55:48 +0000

    Aug 7, 2025, 9:55 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -13,8 +13,15 @@ flapUpImg=loadImage('assets/bluebird-upflap.png');
 function setup(){
 new Canvas(400,600);
 background("gray");
+
 bird=new Sprite();
 bird.img=flapMidImg
+
+
+
+
+
+
 floor= new Sprite();
 floor.collider= "static";
 floor.img=base;
