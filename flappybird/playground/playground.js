874c101c0c18617cbba7d451f84a3889f49bf64c
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2025-08-21 09:32:41 +0000

    Aug 21, 2025, 9:32 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -76,19 +76,17 @@ pipeGroup = new Group();
 
 if(framCount ===1){
 spawnPipePair();
-
 }
 
 
 
 
 
-
+// 
     fill("black")
     textSize(14)
     text('vel.y:'+bird.vel.y.toFixed(2),10,20)
     text('is moving:' + bird.isMoving, 10,40)
     text('sleeping:' + bird.sleeping, 10,60)
-
 
 }// last line
