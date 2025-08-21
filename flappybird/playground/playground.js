1ee64c73ec9175be9ea799c65dea700d6da045e0
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2025-08-21 09:47:55 +0000

    Aug 21, 2025, 9:47 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -99,11 +99,11 @@ let midY=height/2;
 
 
 
-bottomPipe=new Sprite(150, midY+200+gap/2, 52,320,"static");
+bottomPipe=new Sprite(400, midY+200+gap/2, 52,320,"static");
 bottomPipe.img=pipeImg;
 pipeGroup.add(bottomPipe);
 
-topPipe =new Sprite(150,midY-gap/2-200,52,320,"static")
+topPipe =new Sprite(400,midY-gap/2-200,52,320,"static")
 topPipe.img=pipeImg;
 topPipe.rotation=180
 pipeGroup.add(topPipe);
