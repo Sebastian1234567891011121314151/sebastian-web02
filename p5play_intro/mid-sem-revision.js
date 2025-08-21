dabb0821d86da2055222e71074c47e4e9b520717
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2025-08-21 10:48:29 +0000

    Aug 21, 2025, 10:48 AM

--- p5play_intro/mid-sem-revision.js
+++ p5play_intro/mid-sem-revision.js
@@ -1,12 +1,14 @@
 let floor,balls;
 function setup(){
 new Canvas(600,400);
-floor=new Sprite(300,400,600,40)
-
+floor=new Sprite(300,400,600,40);
+world.gravity.y= 10;
 
 
 }
 function draw(){
 background(220);
-
+if (mouse.presses("left")){
+    
+}
 }
