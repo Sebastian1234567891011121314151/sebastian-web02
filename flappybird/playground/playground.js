c8fdca42fee7d42ac139e23f19a6d603cdbe6f39
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2025-08-21 09:26:53 +0000

    Aug 21, 2025, 9:26 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -7,6 +7,7 @@ let flapMidImg,flapDownImg,flapUpImg;
 let pipeGroup;
 let pipeImg;
 let bottomePipe,topPipe;
+
 function preload(){
 bg = loadImage('assets/background-day.png');
 base = loadImage('assets/base.png');
