b09f7a9acd390407f5c66b11f01922ba1536c040
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2025-08-21 09:24:53 +0000

    Aug 21, 2025, 9:24 AM

---
+++ flappybird/playground/pg.playercontrol.js
@@ -0,0 +1,72 @@
+let bird, floor;
+let bg, base;
+let flapMidImg,flapDownImg,flapUpImg;
+function preload(){
+bg = loadImage('assets/background-day.png');
+base = loadImage('assets/base.png');
+flapDownImg=loadImage('assets/bluebird-downflap.png');
+flapMidImg=loadImage('assets/bluebird-midflap.png');
+flapUpImg=loadImage('assets/bluebird-upflap.png');
+}
+function setup(){
+new Canvas(400,600);
+background("gray");
+
+bird=new Sprite();
+bird.img=flapMidImg;
+bird.y=200;
+bird.width=30;
+bird.height=30;
+bird.mass=2;
+bird.drag=0.02;
+bird.bounciness=0.1;
+bird.collider="dynamic"
+
+world.gravity.y= 10;
+
+
+
+
+
+floor= new Sprite();
+floor.collider= "static";
+floor.img=base;
+floor.y=height - 20;
+floor.width= 400;
+floor.height=117;
+mouseY.bouniness=0.9
+}
+
+function draw(){
+    image(bg,0,0,width,height);
+    // if (mouse.presses("left")){
+    //     new Sprite(mouseX,mouseY,30,30,"dynamic")
+    // }
+    // if (mouse.presses("right")){
+    //     new Sprite(mouseX,mouseY,30,30,"static")
+
+    if (kb.presses("space")||mouse.presses("left")){
+        bird.vel.y=-5;
+        bird.sleeping=false;
+    }
+    fill("black")
+    textSize(14)
+    text('vel.y:'+bird.vel.y.toFixed(2),10,20)
+    text('is moving:' + bird.isMoving, 10,40)
+    text('sleeping:' + bird.sleeping, 10,60)
+
+    if(bird.vel.y < -1){
+        bird.img=flapDownImg;
+        bird.rotation= 0;
+    }
+    else if(bird.vel.y > 1) {
+        bird.img=flapUpImg;
+        bird.rotation= 0;
+
+    }
+    else {
+        bird.img=flapMidImg;
+        bird.rotation= 0;
+    }
+
+}
