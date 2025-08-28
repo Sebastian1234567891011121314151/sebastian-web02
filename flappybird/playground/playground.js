efc953be2443b6167aa83f72dcd63ccc33ebb2e1
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2025-08-28 10:46:02 +0000

    Aug 28, 2025, 10:46 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -77,90 +77,82 @@ if (kb.presses("space")||mouse.presses()) {
 
     if(startGame === true){
 
-bird.visible=true;
-bird.collider="dynamic"
-
-
-
-
-
-
+        bird.visible=true;
+        bird.collider="dynamic"
 
     
 
-    if (kb.presses("space")||mouse.presses("left")){
-        bird.vel.y=-5;
-        bird.sleeping=false;
+        if (kb.presses("space")||mouse.presses("left")){
+            bird.vel.y=-5;
+            bird.sleeping=false;
+        }
+
+        if(bird.vel.y < -1){
+            bird.img=flapDownImg;
+            bird.rotation= 0;
+        }
+        else if(bird.vel.y > 1) {
+            bird.img=flapUpImg;
+            bird.rotation= 0;
+
+        }
+        else {
+            bird.img=flapMidImg;
+            bird.rotation= 0;
+        }
+
+        bird.x=bird.x+2;
+        camera.x=bird.x;
+        floor.x=bird.x;
+
+        if (bird.collides(floor) || bird.collides(pipeGroup)){
+            gameOverLabel = new Sprite (width/2, height/2, 192, 42, "none");
+            gameOverLabel.img = gameOverImg
+            gameOverLabel.layer=1000;
+            gameOverLabel.x = camera.x;
+            noLoop();
+        }
+
+
+
+        if(frameCount % 90 === 0){
+        spawnPipePair();
+        }
+
+
+        for (let aPipe of pipeGroup){
+            if (camera.x-aPipe.x >300+25){
+                aPipe.remove()
+            }
+        }
+
+
+        // DEBUGGING CODE
+        fill("black")
+        textSize(14)
+        text('vel.y:'+bird.vel.y.toFixed(2),10,20);
+        text('is moving:' + bird.isMoving, 10,40);
+        text('sleeping:' + bird.sleeping, 10,60);
+        text('pipes count:'+pipeGroup.length, 10,80);
+        text('frame count:'+frameCount, 10,100);
     }
-
-    if(bird.vel.y < -1){
-        bird.img=flapDownImg;
-        bird.rotation= 0;
-    }
-    else if(bird.vel.y > 1) {
-        bird.img=flapUpImg;
-        bird.rotation= 0;
-
-    }
-    else {
-        bird.img=flapMidImg;
-        bird.rotation= 0;
-    }
-
-bird.x=bird.x+2;
-camera.x=bird.x;
-floor.x=bird.x;
-
-if (bird.collides(floor) || bird.collides(pipeGroup)){
-    gameOverLabel = new Sprite (width/2, height/2, 192, 42, "none");
-    gameOverLabel.img = gameOverImg
-    gameOverLabel.layer=1000;
-    gameOverLabel.x = camera.x;
-    noLoop();
-}
-
-
-
-
-
-if(frameCount % 90 === 0){
-spawnPipePair();
-}
-
-
-for (let aPipe of pipeGroup){
-    if (camera.x-aPipe.x >300+25){
-        aPipe.remove()
-    }
-}
-
-
-// DEBUGGING CODE
-    fill("black")
-    textSize(14)
-    text('vel.y:'+bird.vel.y.toFixed(2),10,20);
-    text('is moving:' + bird.isMoving, 10,40);
-    text('sleeping:' + bird.sleeping, 10,60);
-    text('pipes count:'+pipeGroup.length, 10,80);
-    text('frame count:'+frameCount, 10,100);
-
 }// last line
 
 // My custom function
 function spawnPipePair() {
-let gap=30;
-let midY=random(200,350);
+    let gap=30;
+    let midY=random(200,350);
 
-bottomPipe=new Sprite(bird.x+300, midY+200+gap/2, 52,320,"static");
-bottomPipe.img=pipeImg;
-pipeGroup.add(bottomPipe);
+    bottomPipe=new Sprite(bird.x+300, midY+200+gap/2, 52,320,"static");
+    bottomPipe.img=pipeImg;
+    pipeGroup.add(bottomPipe);
 
-topPipe =new Sprite(bird.x+300,midY-gap/2-200,52,320,"static");
-topPipe.img=pipeImg;
-topPipe.rotation=180;
-pipeGroup.add(topPipe);
+    topPipe =new Sprite(bird.x+300,midY-gap/2-200,52,320,"static");
+    topPipe.img=pipeImg;
+    topPipe.rotation=180;
+    pipeGroup.add(topPipe);
 
-pipeGroup.layer=0;
-}
-}
+    pipeGroup.layer=0;
+    }
+
 
