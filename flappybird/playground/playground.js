cae0bf9cda43b387c4cb865117155219a0117cdc
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2025-08-28 10:09:58 +0000

    Aug 28, 2025, 10:09 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -16,6 +16,7 @@ function preload(){
     flapMidImg=loadImage('assets/bluebird-midflap.png');
     flapUpImg=loadImage('assets/bluebird-upflap.png');
     pipeImg=loadImage('assets/pipe-red.png');
+        pipeImg=loadImage('assets/pipe-red.png');
 }// asset loading
 function setup(){
 new Canvas(400,600);
