1fc4a5a2ef38933ffff76dba9a31a172cb8c8b5b
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2025-08-28 10:12:53 +0000

    Aug 28, 2025, 10:12 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -85,7 +85,7 @@ camera.x=bird.x;
 floor.x=bird.x;
 
 if (bird.collides(floor)||bird.collides(pipeGroup)){
-    gameOverLabel = new Sprite (width/)
+    gameOverLabel = new Sprite (width/2, hieght/2, 192)
     noLoop();
 }
 
