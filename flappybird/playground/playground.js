aabb195a3faf84b86dfd0bddc67a23a1cc89e42b
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2025-08-28 10:30:22 +0000

    Aug 28, 2025, 10:30 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -56,7 +56,7 @@ mouseY.bouniness=0.9;
 pipeGroup = new Group();
 
 startScreenLabel= new Sprite(width/2, hieght/2, 184,267,"none");
-startScreenLabel.img=
+startScreenLabel.img= startScreenImg;
 }
 
 function draw(){
@@ -140,4 +140,7 @@ topPipe.rotation=180;
 pipeGroup.add(topPipe);
 
 pipeGroup.layer=0;
+}
+function startscreen(){
+    
 }
