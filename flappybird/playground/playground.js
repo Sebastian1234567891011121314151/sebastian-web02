803cc17504412c64c221e34df6ab5758d3fd9cbc
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2025-08-28 10:41:00 +0000

    Aug 28, 2025, 10:41 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -69,6 +69,11 @@ function draw(){
     // if (mouse.presses("right")){
     //     new Sprite(mouseX,mouseY,30,30,"static")
 
+if (kb.presses("space")||mouse.presses("left"))
+    startGame=true;
+startScreenLabel.visible=false
+
+
     if(startGame === true){
 
 
