c56690cf597653ee4148529713b22d6e35ab3421
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2025-08-28 10:26:30 +0000

    Aug 28, 2025, 10:26 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -23,7 +23,7 @@ function preload(){
 
     gameOverImg=loadImage('assets/gameover.png');
 
-    startScreenImg=loadImage
+    startScreenImg=loadImage('assets/message.png')
 }// asset loading
 function setup(){
 new Canvas(400,600);
