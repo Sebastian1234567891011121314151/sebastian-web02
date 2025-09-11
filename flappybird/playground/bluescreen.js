35473e82b91985e9e1221e99799c0e4919fe335e
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2025-09-11 09:59:02 +0000

    Sep 11, 2025, 9:59 AM

--- flappybird/playground/bluescreen.js
+++ flappybird/playground/bluescreen.js
@@ -81,7 +81,7 @@ function displayScore(){
     let middle= width/2;
     for(let one of scoreDigitArray){
         let onedigit= new scoreGroup.Sprite(middle+offset,height/2,24,26);
-        onedigit.x=
+        onedigit.x=onedigit.x-
         onedigit.img=digitImgs[one]
         offset=offset+25
     }
