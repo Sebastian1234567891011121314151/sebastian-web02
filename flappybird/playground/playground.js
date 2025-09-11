5ef71fe31f566a74236606d83375b44ecc443e6a
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2025-09-11 10:16:03 +0000

    Sep 11, 2025, 10:16 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -232,6 +232,9 @@ function spawnPipePair() {
     topPipe =new Sprite(bird.x+300,midY-gap/2-200,52,320,"static");
     topPipe.img=pipeImg;
     topPipe.rotation=180;
+
+    
+
     pipeGroup.add(topPipe);
 
     pipeGroup.layer=0;
