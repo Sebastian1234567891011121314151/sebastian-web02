5d5b62ff364f705ffa7fb81b131467f98e36ffbb
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2025-09-11 10:04:06 +0000

    Sep 11, 2025, 10:04 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -40,6 +40,17 @@ function preload(){
     gameOverImg=loadImage('assets/gameover.png');
 
     startScreenImg=loadImage('assets/message.png')
+
+        let prefix = "assets/";
+    let postfix= ".png";
+    for (let count=0; count<10; count++){
+        let filename= prefix+ count+ postfix;
+        // console.log(filename);
+        digitImgs[count]= loadImage(filename)
+    }
+
+
+
 }// funtion preload last line
 
 
