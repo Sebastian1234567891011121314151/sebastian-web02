3385c44288dd3233d4060141e31be422b34161f4
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2025-09-11 10:20:57 +0000

    Sep 11, 2025, 10:20 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -170,7 +170,7 @@ if (kb.presses("space")||mouse.presses()) {
         for(let pipe of pipeGroup){
             if (pipe.passed=== false){
                 let leftEdgeBird = bird.x-bird.width/2
-                let RightEdgePipe =pipe.x
+                let RightEdgePipe =pipe.x=pipe.width/2
             }
 
 
