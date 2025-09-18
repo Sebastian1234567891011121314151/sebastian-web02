44f989f30917e7053bee30466a80c147eb8f94f8
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2025-09-18 09:37:11 +0000

    Sep 18, 2025, 9:37 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -42,8 +42,8 @@ function preload(){
     startScreenImg=loadImage('assets/message.png')
 
     flapSound= createAudio('assets/sfx_wing.mp3')
-    flapSound= createAudio('assets/sfx_wing.mp3')
-    flapSound= createAudio('assets/sfx_wing.mp3')
+    pointSound= createAudio('assets/sfx_wing.mp3')
+    failSound= createAudio('assets/sfx_wing.mp3')
 
 
 
