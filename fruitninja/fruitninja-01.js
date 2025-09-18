5f291ec007c5180a63c60a90e6a9c52cb5a63c20
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2025-09-18 10:31:22 +0000

    Sep 18, 2025, 10:31 AM

--- fruitninja/fruitninja-01.js
+++ fruitninja/fruitninja-01.js
@@ -17,8 +17,23 @@ function setup(){
 
 function draw(){
 image(bg,0,0,width,height)
-
+displayGO()
 }//last line
 
 
-function displayGO()
+function displayGO(){
+    let textmid =width/2
+
+textAlign(CENTER , CENTER)
+
+
+
+textSize(45)
+fill("red")
+text('Game Over!',textmid,height/2)
+textSize(25)
+fill("white")
+text('Score : '+score,textmid,350)
+text('Missed Fruits : '+missedfruit,textmid,400)
+text('Press SPACE to Restart',textmid,450)
+}
