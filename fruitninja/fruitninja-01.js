9eebfe665380458132eb848b54f9831ddccf8554
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2025-09-18 10:30:24 +0000

    Sep 18, 2025, 10:30 AM

--- fruitninja/fruitninja-01.js
+++ fruitninja/fruitninja-01.js
@@ -1,6 +1,6 @@
 let bg;
 let score=1
-let 
+let missedfruit=10
 function preload(){
     bg=loadImage('assets/dojobackground.png');
 }
@@ -28,7 +28,7 @@ fill("red")
 text('Game Over!',textmid,height/2)
 textSize(25)
 fill("white")
-text('Score : ',textmid,350)
+text('Score : '+score,textmid,350)
 text('Missed Fruits : ',textmid,400)
 text('Press SPACE to Restart',textmid,450)
 }//last line
