71ff5e4e108217cb6e622fcd9e1bf96f459d82bb
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2025-09-18 09:48:41 +0000

    Sep 18, 2025, 9:48 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -239,7 +239,9 @@ if (kb.presses("space")||mouse.presses()) {
         if(frameCount % 90 === 0){
         spawnPipePair();
         }
-        if (frameCount% 60 ===0)
+        if (frameCount% 60 ===0){
+            
+        }
 
 
         for (let aPipe of pipeGroup){
