4098cf99e496dd36b221f3aab279b934a11b2f00
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2025-09-18 10:50:41 +0000

    Sep 18, 2025, 10:50 AM

--- fruitninja/fruitninja-01.js
+++ fruitninja/fruitninja-01.js
@@ -26,19 +26,19 @@ function draw(){
 
 
 image(bg,0,0,width,height);
-// 
+
 gameStart2()
 
 if (kb.presses('space')){
  gameStart=true;
 }
 
-if(gameStart){
-    displayPLAY();
-}
-else{
-displayGO();
-}
+// if(gameStart){
+//     displayPLAY();
+// }
+// else{
+// displayGO();
+// }
 
 
 
