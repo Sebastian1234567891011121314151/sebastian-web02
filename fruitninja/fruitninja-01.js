7ec201c200fa6a5a663eec46b9ab9aac75bcb16b
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2025-09-18 10:24:02 +0000

    Sep 18, 2025, 10:24 AM

--- fruitninja/fruitninja-01.js
+++ fruitninja/fruitninja-01.js
@@ -23,6 +23,6 @@ text('Game Over!', 400,300)
 textSize(25)
 fill("white")
 text('Score : ',450,350)
-text('Missed Fruits : ',500,350)
-text('Press SPACE to Restart',550,350)
+text('Missed Fruits : ',450,400)
+text('Press SPACE to Restart',450,450)
 }//last line
