500dce776e58a4150070437baf9d508cc873df94
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2025-09-25 10:11:03 +0000

    Sep 25, 2025, 10:11 AM

--- fruitninja/fruitninja-02.js
+++ fruitninja/fruitninja-02.js
@@ -121,7 +121,7 @@ textSize(25);
 fill("white");
 text('Missed Fruits : '+missedfruit, width/2,textmidhoriz);
 textAlign(LEFT,CENTER )
-text('Score : '+score,150,textmidhoriz);
+text('Score : '+score,10,textmidhoriz);
 textAlign(LEFT,CENTER )
 text('Time : '+timer+' sec',800,textmidhoriz);
 }
