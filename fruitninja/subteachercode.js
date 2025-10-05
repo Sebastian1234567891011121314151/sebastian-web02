c1fa1918da173592b2c78dad8a7b9e5428229a0f
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2025-10-05 03:25:37 +0000

    Oct 5, 2025, 3:25 AM

---
+++ fruitninja/subteachercode.js
@@ -0,0 +1,46 @@
+let fruitGroup;
+let fruitTypes =[];
+let dogoBG;
+function preload(){
+     dogoBG = loadImage('assets/dojobackground.png');
+    //declare peach 
+    let peach = {
+        whole:loadImage('assets/peachwhole.png')
+    };
+    let watermelon = {
+        whole: loadImage('assets/watermelonwhole.png')
+    }
+    //store the fruit objject into fruitTypes array[]
+    fruitTypes = [peach,watermelon];
+}
+function setup(){
+    new Canvas(800,600);
+    //set gravity to 10
+    world.gravity.y = 10;
+    fruitGroup= new Group();
+}
+function draw(){
+    clear();
+    image(dogoBG,0,0,width,height);
+    // % refers to the remainder so if framecount / by 120 and the remainder is 0 then i call spawnfruit function
+    if(frameCount % 120 === 0){
+        spawnFruit();
+    }
+    if(mouse.pressing()){
+        trail = new Sprite(mouse.x,mouse.y,7);
+        trail.collider = 'none';
+        trail.color ="red";
+        trail.life = 10;
+    }
+}
+
+function spawnFruit(){
+    let fruitData = random(fruitTypes);
+    let randomX = random(300,500);
+    let fruit = new fruitGroup.Sprite(randomX,600+20,40); 
+    fruit.image = fruitData.whole;
+    fruit.type = fruitData;
+    fruit.vel.y = random(-10,-14);
+    fruit.vel.x = random(-2,2);
+    fruit.friction = 0;
+}
