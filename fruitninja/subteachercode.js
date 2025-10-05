b02d4aca726d73bc283fa5a2fed314fad1409cd4
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2025-10-05 03:53:49 +0000

    Oct 5, 2025, 3:53 AM

--- fruitninja/subteachercode.js
+++ fruitninja/subteachercode.js
@@ -73,5 +73,6 @@ function splitFruit(x,y,fruitData){
 let left=new fruitHalves.Sprite(x-10,y,40,40);
 left.img = fruitData.half;
 left.vel.x=-3
-left.vel.y=random(-5,-2)
+left.vel.y=random(-5,-2);
+left.rotationSpeed
 }
