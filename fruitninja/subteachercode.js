d4209d1abc57ef26a51b3f31976e0b18c958331e
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2025-10-05 04:16:07 +0000

    Oct 5, 2025, 4:16 AM

--- fruitninja/subteachercode.js
+++ fruitninja/subteachercode.js
@@ -35,8 +35,8 @@ function draw(){
     if(mouse.pressing()){
         trail = new Sprite(mouse.x,mouse.y,7);
         trail.collider = 'none';
-        trail.color ="red";
-        trail.life = 10;
+        trail.color ="white";
+        trail.life = 100;
         sliceFruit();
     }
    
