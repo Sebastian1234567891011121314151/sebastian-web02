bf12b4e9d557b346026e46f8954311a2ce15ba55
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2025-10-16 10:00:48 +0000

    Oct 16, 2025, 10:00 AM

--- fruitninja/practice-code.js
+++ fruitninja/practice-code.js
@@ -1,3 +1,21 @@
-let fruitshop ={
-    apple: 0.3
+function setup(){
+    let fruitshop ={
+        apple: 0.3,
+        pineapple:1.6,
+        grape:0.5,
+        kiwi:0.25
+    }
+    console.log(fruitshop);
+    console.log(fruitshop.apple);
+
+    fruitshop.dragonfruit = 0.7
+    console.log(fruitshop)
+
+
+
 }
+
+function draw(){
+
+}
+
