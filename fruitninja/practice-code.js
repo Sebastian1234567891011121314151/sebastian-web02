662bc178604dcdbd155f5a0b13503b1ebdb52ed2
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2025-10-16 10:40:09 +0000

    Oct 16, 2025, 10:40 AM

--- fruitninja/practice-code.js
+++ fruitninja/practice-code.js
@@ -1,41 +1,41 @@
 function setup(){
-    let fruitshop ={
-        apple: 0.3,
-        pineapple:1.6,
-        grape:0.5,
-        kiwi:0.25
-    }
-    console.log(fruitshop);
-    console.log(fruitshop.apple);
+    // let fruitshop ={
+    //     apple: 0.3,
+    //     pineapple:1.6,
+    //     grape:0.5,
+    //     kiwi:0.25
+    // }
+    // console.log(fruitshop);
+    // console.log(fruitshop.apple);
 
-    fruitshop.dragonfruit = 0.7
-    console.log(fruitshop)
-
-
-    let apple ={
-        name:"Apple",
-        price:0.4,
-        quantity:50
-    };
-
-    let lemon ={
-        name:"Lemon",
-        price:0.2,
-        quantity:100
-    };
-    let watermelon ={
-        name:"Watermelon",
-        price:1.3,
-        quantity:20
-    };
+    // fruitshop.dragonfruit = 0.7
+    // console.log(fruitshop)
 
 
+    // let apple ={
+    //     name:"Apple",
+    //     price:0.4,
+    //     quantity:50
+    // };
 
-    console.log(apple);
-    let shopkeeper = [apple,lemon,watermelon];
-    let randomFruit = random(shopkeeper)
-    console.log(shopkeeper)
-    console.log(randomFruit)
+    // let lemon ={
+    //     name:"Lemon",
+    //     price:0.2,
+    //     quantity:100
+    // };
+    // let watermelon ={
+    //     name:"Watermelon",
+    //     price:1.3,
+    //     quantity:20
+    // };
+
+
+
+    // console.log(apple);
+    // let shopkeeper = [apple,lemon,watermelon];
+    // let randomFruit = random(shopkeeper)
+    // console.log(shopkeeper)
+    // console.log(randomFruit)
 
 
 }
