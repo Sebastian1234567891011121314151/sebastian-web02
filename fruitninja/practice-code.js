8f270a45c91f9a5dde74cc675c8d791d18da8f2a
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2025-10-23 10:29:51 +0000

    Oct 23, 2025, 10:29 AM

--- fruitninja/practice-code.js
+++ fruitninja/practice-code.js
@@ -1,76 +1,76 @@
 
-let watermelon
-let watermelonimg
-let bg
+// let watermelon
+// let watermelonimg
+// let bg
 
-function preload(){
-    watermelonimg=loadImage('assets/watermelonwhole.png')
-    bg=loadImage('assets/dojobackground.png')
+// function preload(){
+//     watermelonimg=loadImage('assets/watermelonwhole.png')
+//     bg=loadImage('assets/dojobackground.png')
     
-}
+// }
 
 
-function setup(){
-    world.gravity.y=10
-    new Canvas(400,600)
-    background("white")
+// function setup(){
+//     world.gravity.y=10
+//     new Canvas(400,600)
+//     background("white")
 
-    watermelon=new Sprite(200,300,50);
-    watermelon.img=watermelonimg
-
-
-
-
-    // let fruitshop ={
-    //     apple: 0.3,
-    //     pineapple:1.6,
-    //     grape:0.5,
-    //     kiwi:0.25
-    // }
-    // console.log(fruitshop);
-    // console.log(fruitshop.apple);
-
-    // fruitshop.dragonfruit = 0.7
-    // console.log(fruitshop)
-
-
-    // let apple ={
-    //     name:"Apple",
-    //     price:0.4,
-    //     quantity:50
-    // };
-
-    // let lemon ={
-    //     name:"Lemon",
-    //     price:0.2,
-    //     quantity:100
-    // };
-    // let watermelon ={
-    //     name:"Watermelon",
-    //     price:1.3,
-    //     quantity:20
-    // };
-
-
-
-    // console.log(apple);
-    // let shopkeeper = [apple,lemon,watermelon];
-    // let randomFruit = random(shopkeeper)
-    // console.log(shopkeeper)
-    // console.log(randomFruit)
-
-
-}
+//     watermelon=new Sprite(200,300,50);
+//     watermelon.img=watermelonimg
 
 
 
 
+//     // let fruitshop ={
+//     //     apple: 0.3,
+//     //     pineapple:1.6,
+//     //     grape:0.5,
+//     //     kiwi:0.25
+//     // }
+//     // console.log(fruitshop);
+//     // console.log(fruitshop.apple);
 
-function draw(){
-    image(bg,0,0,width,height)
-    if (mouse.presses()){
-        watermelon.velocity.y=-7
-}
+//     // fruitshop.dragonfruit = 0.7
+//     // console.log(fruitshop)
 
-}
+
+//     // let apple ={
+//     //     name:"Apple",
+//     //     price:0.4,
+//     //     quantity:50
+//     // };
+
+//     // let lemon ={
+//     //     name:"Lemon",
+//     //     price:0.2,
+//     //     quantity:100
+//     // };
+//     // let watermelon ={
+//     //     name:"Watermelon",
+//     //     price:1.3,
+//     //     quantity:20
+//     // };
+
+
+
+//     // console.log(apple);
+//     // let shopkeeper = [apple,lemon,watermelon];
+//     // let randomFruit = random(shopkeeper)
+//     // console.log(shopkeeper)
+//     // console.log(randomFruit)
+
+
+// }
+
+
+
+
+
+// function draw(){
+//     image(bg,0,0,width,height)
+//     if (mouse.presses()){
+//         watermelon.velocity.y=-7
+// }
+
+// }
 
