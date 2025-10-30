19375b959077bc37b881bd55359e5cb2ed0154c0
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2025-10-30 10:47:21 +0000

    Oct 30, 2025, 10:47 AM

--- fruitninja/practice-code.js
+++ fruitninja/practice-code.js
@@ -284,7 +284,7 @@ let Me ={name:"Me",gender:'M',height:164}
 
 
 
-friends=[TimeCheese,Tim,Cheese,TimChe,Chee,Teacher,Me]
+friends=[TimCheese,Tim,Cheese,TimChe,Chee,Teacher,Me]
 
 for(let one of friends){
   if(gender="M")
