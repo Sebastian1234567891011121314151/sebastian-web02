67504eced91e02072d5bbfc88348a5ad38999634
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2025-11-06 10:45:17 +0000

    Nov 6, 2025, 10:45 AM

--- p5play_intro/wordlegame.js
+++ p5play_intro/wordlegame.js
@@ -23,7 +23,7 @@ button1.mousePressed( handleUserInput);
 
 function handleUserInput(){
 if (guess === hidden){
-    message()
+    message("nice u got it")
 }
 // write if condition
 // to check if the guess is matching to the hidden word
