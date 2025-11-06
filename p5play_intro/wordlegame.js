5f8491a74ccc5b7695053e10a32aebe5d2b24c52
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2025-11-06 10:04:38 +0000

    Nov 6, 2025, 10:04 AM

--- p5play_intro/wordlegame.js
+++ p5play_intro/wordlegame.js
@@ -1,20 +1,20 @@
 
 
 
-let attempts=0
+let attempts=0;
 
 function setup() {
-new Canvas (600,500)
-background('pink')
+new Canvas (600,500);
+background('pink');
 }
 
 function draw()  {
-    
-textAlign(CENTER,CENTER )
+    clear();
+textAlign(CENTER,CENTER );
     textSize(25);
     fill("black");
-    text('Guess the hidden Word!' ,width/2,150)
-    text('Attemps: '+attempts, width/2,190)
-    text('Word : T _ _ _ _',width/2,230)
+    text('Guess the hidden Word!' ,width/2,150);
+    text('Attemps: '+attempts, width/2,190);
+    text('Word : T _ _ _ _',width/2,230);
 
 }
