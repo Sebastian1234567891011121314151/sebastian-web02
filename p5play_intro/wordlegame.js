167fc10530c08217d1c3b99c9ec6e4167a82c359
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2025-11-06 10:29:12 +0000

    Nov 6, 2025, 10:29 AM

--- p5play_intro/wordlegame.js
+++ p5play_intro/wordlegame.js
@@ -12,7 +12,7 @@ hidden= random(LIBRARY)
 console.log(hidden)
 hint=hidden[0] +'_ '.repeat(hidden.length-1)
 guessInput= createInput("")
-guessInput.position(width/2-1000,370)
+guessInput.position(width/2-100,370)
 }
 
 function draw()  {
